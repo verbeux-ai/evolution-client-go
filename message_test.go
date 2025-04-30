8f77dc2b0a9f26aa18c7f92ec4c54d7f3708af3f
Pedro Ivo <pi3virgula14@outlook.com>
2025-04-29 22:21:27 -0300

    feat: add button list

--- message_test.go
+++ message_test.go
@@ -29,3 +29,28 @@ func TestSendAudioMessage(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, result)
 }
+
+func TestSendListMessage(t *testing.T) {
+	ctx := context.Background()
+	result, err := client.SendListMessage(ctx, os.Getenv("INSTANCE_NAME"), &evolution.ListMessageRequest{
+		Number:      os.Getenv("NUMBER"),
+		Title:       "Testando",
+		Description: "Descricao",
+		ButtonText:  "Botao text",
+		FooterText:  "Footer text",
+		Sections: []evolution.ListMessageRequestSection{
+			{
+				Title: "teste title",
+				Rows: []evolution.ListMessageRequestSectionRow{
+					{
+						Title:       "teste row title",
+						Description: "teste row description",
+						RowID:       "1",
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
+}
